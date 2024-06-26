5e27551846f4e5b647eed440c5eea33810c6dca2
Kevon Cooper <imkvncpr@gmail.com>
2024-06-26 06:24:59 -0400

    first commit

---
+++ AI-Assistant.py
@@ -0,0 +1,62 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+
+engine = pyttsx3.init()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    
+speak("This is the AI assistant")
+
+
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak(Time)
+
+time()
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    day = int(datetime.datetime.now().day)
+    speak(day)
+    speak(month)
+    speak(year)
+date()
+
+def greeting():
+    speak("Greetings")
+    speak("The present time is")
+    time()
+    speak("Todays date is currently")
+    date()
+    hour = datetime.datetime.now().hour
+    if hour >=6 and hour <12:
+        speak("Good Morning")
+    elif hour >=12 and hour <18:
+        speak("Good Afternoon")
+    elif hour >=18 and hour <24:
+        speak("Good Evening")
+    else:
+        speak("Good Night")
+        
+    speak("I am here to assist")
+    
+greeting()
+
+def voiceCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pauseThreshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google_cloud(audio language="en-us")
+        print(query)
+           
+    except Exception as e:
+        print(e)
+        
